68971c44467d1b5aec2784d7377954024be99fde
Соколовский Иван Павлович <ivan3177@gmail.com>
2024-01-09 17:49:47 +0400

    Integrate TodosListScreen component into App
    A TodosListScreen component has been imported and integrated into the App component, nested within the WithProviders. The code update enhances the app's functionality by providing a dedicated screen for managing tasks.

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import {WithProviders} from './withProviders.tsx';
+import {TodosListScreen} from '../screens';
 
 export const App: React.FunctionComponent = () => {
-  return <WithProviders>{}</WithProviders>;
+  return (
+    <WithProviders>
+      <TodosListScreen />
+    </WithProviders>
+  );
 };
